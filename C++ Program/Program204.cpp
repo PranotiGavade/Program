d7cf8a80ce54a4febf8a7a4abbb64a77c912454f
PranotiGavade <pranotigavade19@gmail.com>
2024-09-13 11:47:28 +0530

    Add files via upload

---
+++ C++ Program/Program204.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int SumFactors(int iNo)
+{
+    int iSum = 0;
+    int iCnt = 0;
+    for(iCnt = 0; iCnt <= iNo / 2; iCnt++)
+    {
+        if((iNo % iCnt == 0))
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+}
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    cout<<"Enter number\n";
+    cin>>iValue;
+
+    iRet = SumFactors(iValue);
+
+    cout<<"Summation of factors is : "<<iRet<<endl;
+    return 0;
+}
