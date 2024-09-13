2138c899a5090aa4839631a0ab3f26e5b2303d63
PranotiGavade <pranotigavade19@gmail.com>
2024-09-13 11:46:47 +0530

    Add files via upload

---
+++ C++ Program/Program216.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iLength = 0, i = 0;
+    int *ptr = NULL;
+
+    cout<<"Enter number of elements that you want to store :"<<endl;
+    cin>>iLength;
+
+    ptr = new int[iLength];
+    
+    return 0;
+}
