10496be432a60cf0a92082f79b133be8a8845f06
PranotiGavade <pranotigavade19@gmail.com>
2024-09-13 12:42:01 +0530

    Add files via upload

---
+++ Java Programs/Program677.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Program677
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String :");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+"," ");
+
+        System.out.println(str);
+
+        String Arr[] = str.split(" ");
+
+        HashMap <String,Integer> hobj = new HashMap<String,Integer>();
+
+        int Frequency = 0;
+
+        for(String A : Arr)
+        {
+            if(hobj.containsKey(A))     //string is already present
+            {
+                Frequency = hobj.get(A);
+                hobj.put(A,Frequency+1);
+            }
+            else        //string occurs first time
+            {
+                hobj.put(A,1);
+            }
+        }
+
+        Set <String> setobj = hobj.keySet();
+
+        for(String B : setobj)
+        {
+            System.out.println(B +" Occurs "+hobj.get(B)+" time");
+        }
+    }
+}
